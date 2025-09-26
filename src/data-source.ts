09cfd573d64b9c05e14041709fb81af25c9c3c9c
ab989 <wowsilverb@outlook.com>
2025-09-26 18:29:41 +0200

    Add env variables for hosted postgres

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,13 +3,19 @@ import { DataSource } from "typeorm";
 import { Book } from "./entities/Book";
 import { Author } from "./entities/Author";
 
+const DB_HOST = process.env.DATABASE_HOST || "localhost"
+const DB_PORT = Number(process.env.DATABASE_PORT) || 5431
+const DB_USERNAME = process.env.DATABASE_USERNAME || "myuser"
+const DB_PASSWORD = process.env.DATABASE_PASSWORD || "mypassword"
+const DB_NAME = process.env.DATABASE_NAME || "mydatabase"
+
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5431,
-    username: "myuser",
-    password: "mypassword",
-    database: "mydatabase",
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
     synchronize: true,
     logging: false,
     entities: [Book, Author],
