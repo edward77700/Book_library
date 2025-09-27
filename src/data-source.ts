f68b05435f4f531f544e1a53016f3929bdfd9a1b
AndriiMandziuk <edwardthomas7770@gmail.com>
2025-09-27 18:27:57 +0200

    update env

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,11 +3,11 @@ import { DataSource } from "typeorm";
 import { Book } from "./entities/Book";
 import { Author } from "./entities/Author";
 
-const DB_HOST = process.env.DATABASE_HOST || "localhost"
-const DB_PORT = Number(process.env.DATABASE_PORT) || 5431
-const DB_USERNAME = process.env.DATABASE_USERNAME || "myuser"
-const DB_PASSWORD = process.env.DATABASE_PASSWORD || "mypassword"
-const DB_NAME = process.env.DATABASE_NAME || "mydatabase"
+const DB_HOST = "postgres.railway.internal"
+const DB_PORT = 5432
+const DB_USERNAME = "postgres"
+const DB_PASSWORD = "CeYYbEZXfCmJGJFHMNnQrMHSXqPcvXyY"
+const DB_NAME = "railway"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
